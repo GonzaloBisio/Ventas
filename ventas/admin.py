c4df3ecf82af761089369ec21435318cf461bd63
GonzaloBisio2002.Gonzalo <GonzaloBisio2002.crackbisio@gmail.com>
2020-07-01 00:02:03 -0300

    Models terminados, customizando admin

--- ventas/admin.py
+++ ventas/admin.py
@@ -4,7 +4,18 @@ from .models import *
 # Register your models here.
 admin.site.register(Venta)
 admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Proveedore)
-admin.site.register(Cliente)
 admin.site.register(Direccion)
+
+@admin.register(Cliente)
+class ClienteAdmin (admin.ModelAdmin):
+    list_display = ['rut' , 'nombre', 'telefono']
+
+@admin.register(Producto)
+class ProductoAdmin (admin.ModelAdmin):
+     fieldset = (('Descripción',{'fields ' :('idd', 'nombre')}), ('Variables', {'fields': ('precio' , 'stock')}))
+
+@admin.register(Proveedore)
+class ProveedoreFilter(admin.ModelAdmin):
+    class Meta:
+        model = Proveedore
+        fields = ['nombre', 'rut']  
--- ventas/models.py
+++ ventas/models.py
@@ -4,52 +4,53 @@ from django.db import models
 
 
 class Producto(models.Model):
-    idd = models.CharField(max_length = 20)
-    nombre = models.CharField(max_length = 20)
-    precio = models.CharField(max_length = 20)
+    idd = models.CharField(max_length = 50, null=False)
+    nombre = models.CharField(max_length = 50, null=False)
+    precio = models.CharField(max_length = 50, null=False)
+    stock = models.IntegerField(default = 0)
     categoria = models.ForeignKey("Categoria", related_name='+',  on_delete=models.CASCADE,null = False,)
     proveedor = models.ForeignKey("Proveedore", related_name='+',  on_delete=models.CASCADE,null = False,)
     def __str__(self):
-        return str(self.nombre)
+        return self.nombre
 
 class Categoria(models.Model):
-    idd = models.CharField(max_length = 20)
-    nombre = models.CharField(max_length = 20)
-    descripcion = models.CharField(max_length = 20)
+    idd = models.CharField(max_length = 50, null=False)
+    nombre = models.CharField(max_length = 50, null=False)
+    descripcion = models.CharField(max_length = 50, null=False)
     def __str__(self):
-        return str(self.nombre)
+        return self.nombre
 
 class Direccion(models.Model):
-    calle = models.CharField(max_length = 20)
-    numero = models.CharField(max_length = 20)
-    comuna = models.CharField(max_length = 20)
-    ciudad = models.CharField(max_length = 20)
+    calle = models.CharField(max_length = 50, null=False)
+    numero = models.CharField(max_length = 50, null=False)
+    comuna = models.CharField(max_length = 50, null=False)
+    ciudad = models.CharField(max_length = 50, null=False)
     def __str__(self):
-        return str(self.ciudad, self.calle)
+        return self.calle + ", " + self.ciudad
+
 
 class Proveedore(models.Model):
-    nombre = models.CharField(max_length = 20)
-    web = models.CharField(max_length = 20)
-    rut = models.CharField(max_length = 20)
-    telefono = models.CharField(max_length = 20)
+    nombre = models.CharField(max_length = 50, null=False)
+    web = models.CharField(max_length = 50, null=False)
+    rut = models.CharField(max_length = 50, null=False)
+    telefono = models.CharField(max_length = 50, null=False)
     direccion = models.ForeignKey("Direccion", related_name='+',  on_delete=models.CASCADE,null = False,)
-    producto = models.ForeignKey("Producto", related_name='+', on_delete=models.CASCADE,null = False,)
     def __str__(self):
-        return str(self.nombre)
+        return self.nombre
 
 class Venta(models.Model):
-    idd = models.CharField(max_length = 20)
-    fecha = models.CharField(max_length = 20)
-    montofinal = models.CharField(max_length = 20)
-    descuento = models.CharField(max_length = 20)
+    idd = models.CharField(max_length = 50, null=False)
+    fecha = models.CharField(max_length = 50, null=False)
+    montofinal = models.CharField(max_length = 50, null=False)
+    descuento = models.CharField(max_length = 50, null=False)
     cliente = models.ForeignKey("Cliente", related_name='+',  on_delete=models.CASCADE,null = False,)
     def __str__(self):
-        return str(self.fecha)
+        return self.fecha + ", " + self.cliente
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length = 20)
-    rut = models.CharField(max_length = 20)
-    telefono = models.CharField(max_length = 20)
+    nombre = models.CharField(max_length = 50, null=False)
+    rut = models.CharField(max_length = 50, null=False)
+    telefono = models.CharField(max_length = 50, null=False)
     direccion = models.ForeignKey("Direccion", related_name='+',  on_delete=models.CASCADE,null = False,)
     def __str__(self):
-        return str(self.nombre)
+        return self.nombre
